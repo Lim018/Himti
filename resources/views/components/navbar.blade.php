4471e97ffb776f2773e8a198326a22ea8fbcf742
Jamaalul Lail <a.jamaalul@gmail.com>
2025-05-01 19:34:48 +0700

    Membuat view compro

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav>
+    <div>
+        <img src="/assets/compro/logo_himpunan.png" alt="logo_himpunan" id="logo_himpunan_nav">
+        <img src="/assets/compro/logo_kabinet.png" alt="logo_kabinet" id="logo_kabinet_nav">
+        <h3>HIMA<br><b>TEKNIK INFORMATIKA</b></h3>
+    </div>
+    <div>
+            <a href="">News</a>
+            <a href="">About Us</a>
+            <a href="">Features</a>
+    </div>
+</nav>
---
+++ resources/views/compro.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HIMTI | Company Profile</title>
+    <link rel="stylesheet" href="css/compro.css">
+    <link rel="stylesheet" href="css/global.css">
+</head>
+<body>
+    @include('components.navbar')
+    <section id="hero">
+        <h1>HIMPUNAN MAHASISWA<br>TEKNIK INFORMATIKA D4</h1>
+        <p>Kabinet Selaras</p>
+        <button></button>
+        <div id="img1"></div>
+        <div id="img2"></div>
+        <img src="/images/compro/compro_img3.png" id="img3" alt="img3">
+        <img src="/assets/compro/logo_kabinet.png" alt="logo_kabinet" id="logo_kabinet">
+    </section>
+    <section id="desc">
+        <h3>KABINET</h3>
+        <h1>SELARAS</h1>
+        <p id="motto">"Bersatu dalam Kolaborasi untuk Inovasi"</p>
+        <div id="photoframe"></div>
+        <div id="textbox">
+            <p>Kabinet "Selaras" lahir dari keinginan untuk menciptakan harmoni antara inovasi dan kolaborasi.<br><br>Kami percaya bahwa keseimbangan antara internal dan eksternal, strategi dan eksekusi, serta perkembangan individu dan kemajuan kolektif adalah kunci untuk membangun ekosistem yang kompetitif dan adaptif.</p>
+        </div>
+        <img src="/images/compro/compro_img4.png" alt="kadep" id="kadep">
+        <img src="/assets/compro/logo_kabinet.png" alt="logo_kabinet" id="logo_kabinet_desc">
+    </section>
+    <section id="visi">
+        <img src="/images/compro/compro_img5.svg" alt="ribbon" id="ribbon">
+        <div id="visileft">
+            <p>HIMTI</p>
+            <h1>VISI</h1>
+        </div>
+        <div id="visiright">
+            <p>HIMTI sebagai pusat pergerakan dan pengembangan mahasiswa, berkomitmen menjadi organisasi yang dikenal baik oleh masyarakat umum dan seluruh warga Fakultas Vokasi, serta membudayakan nilai-nilai integritas dan komitmen dalam berorganisasi.</p>
+        </div>
+    </section>
+</body>
+</html>
