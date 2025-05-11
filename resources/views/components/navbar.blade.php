438d0db3c41febaaa0453d402b2b3d39f24e5fc4
Jamaalul Lail <a.jamaalul@gmail.com>
2025-05-11 08:00:21 +0700

    Menyelesaikan View Compro

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,12 +1,21 @@
-<nav>
-    <div>
-        <img src="/assets/compro/logo_himpunan.png" alt="logo_himpunan" id="logo_himpunan_nav">
-        <img src="/assets/compro/logo_kabinet.png" alt="logo_kabinet" id="logo_kabinet_nav">
-        <h3>HIMA<br><b>TEKNIK INFORMATIKA</b></h3>
+<link rel="stylesheet" href="css/navbar/desktop.css" media="(min-width: 1025px)">
+<link rel="stylesheet" href="css/navbar/tablet.css" media="(min-width: 601px) and (max-width: 1024px)">
+<link rel="stylesheet" href="css/navbar/mobile.css" media="(max-width: 600px)">
+
+<nav id="nav">
+    <div id="navdiv">
+        <div>
+            <img src="/assets/logo_himpunan.png" alt="logo_himpunan" id="logo_himpunan_nav">
+            <img src="/assets/logo_kabinet.png" alt="logo_kabinet" id="logo_kabinet_nav">
+            <h3>HIMA<br><b>TEKNIK INFORMATIKA</b></h3>
+        </div>
+        
     </div>
-    <div>
-            <a href="">News</a>
-            <a href="">About Us</a>
-            <a href="">Features</a>
+    <div id="menudiv">
+            <a href="" class="menu">News</a>
+            <a href="" class="menu">About Us</a>
+            <a href="" class="menu">Features</a>
     </div>
 </nav>
+
+<script src="js/navbar.js"></script>
--- resources/views/compro.blade.php
+++ resources/views/compro.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HIMTI | Company Profile</title>
-    <link rel="stylesheet" href="css/compro.css">
+    <link rel="stylesheet" href="css/compro/desktop.css" media="(min-width: 1025px)">
+    <link rel="stylesheet" href="css/compro/tablet.css" media="(min-width: 601px) and (max-width: 1024px)">
+    <link rel="stylesheet" href="css/compro/mobile.css" media="(max-width: 1024px)">
     <link rel="stylesheet" href="css/global.css">
 </head>
 <body>
@@ -13,11 +15,11 @@
     <section id="hero">
         <h1>HIMPUNAN MAHASISWA<br>TEKNIK INFORMATIKA D4</h1>
         <p>Kabinet Selaras</p>
-        <button></button>
+        <button id="herobutton">Button</button>
         <div id="img1"></div>
         <div id="img2"></div>
+        <img src="/assets/logo_kabinet_transparan.png" alt="logo_kabinet" id="logo_kabinet">
         <img src="/images/compro/compro_img3.png" id="img3" alt="img3">
-        <img src="/assets/compro/logo_kabinet.png" alt="logo_kabinet" id="logo_kabinet">
     </section>
     <section id="desc">
         <h3>KABINET</h3>
@@ -28,7 +30,7 @@
             <p>Kabinet "Selaras" lahir dari keinginan untuk menciptakan harmoni antara inovasi dan kolaborasi.<br><br>Kami percaya bahwa keseimbangan antara internal dan eksternal, strategi dan eksekusi, serta perkembangan individu dan kemajuan kolektif adalah kunci untuk membangun ekosistem yang kompetitif dan adaptif.</p>
         </div>
         <img src="/images/compro/compro_img4.png" alt="kadep" id="kadep">
-        <img src="/assets/compro/logo_kabinet.png" alt="logo_kabinet" id="logo_kabinet_desc">
+        <img src="/assets/logo_kabinet_transparan.png" alt="logo_kabinet" id="logo_kabinet_desc">
     </section>
     <section id="visi">
         <img src="/images/compro/compro_img5.svg" alt="ribbon" id="ribbon">
@@ -40,5 +42,57 @@
             <p>HIMTI sebagai pusat pergerakan dan pengembangan mahasiswa, berkomitmen menjadi organisasi yang dikenal baik oleh masyarakat umum dan seluruh warga Fakultas Vokasi, serta membudayakan nilai-nilai integritas dan komitmen dalam berorganisasi.</p>
         </div>
     </section>
+    <section id="misi">
+        <div id="misileft">
+            <p>HIMTI</p>
+            <h1>MISI</h1>
+        </div>
+        <div id="misiright">
+            <p>Membangun dan memperkuat budaya komunikasi dalam organisasi, dengan memastikan bahwa setiap anggota terlibat aktif, memiliki tanggung jawab, dan berkontribusi secara positif.</p>
+            <p>Membangun citra positif HIMTI melalui berbagai kegiatan publik, menyebarkan dampak positif yang dihasilkan, serta memperkenalkan kontribusi organisasi kepada masyarakat umum dan seluruh komunitas akademik di Fakultas Vokasi.</p>
+            <p>Melaksanakan program kerja berlandaskan profesionalitas dan berbasis kekeluargaan dalam berorganisasi dan bermasyarakat.</p>
+        </div>
+    </section>
+    <section id="filosofi">
+        <div id="flsftop">
+            <p>FILOSOFI</p>
+            <h1>LOGO</h1>
+        </div>
+        <div id="flsfbottom">
+            <div id="flsfleft"></div>
+            <div id="flsfright">
+                <div class="flsfcards">
+                    <div class="cardtop">
+                        <img src="/images/compro/compro_img6.jpg" alt="shape" id="shape">
+                        <h3>Shapes &amp; Compositions</h3>
+                    </div>
+                    <div class="cardbottom">
+                        <p>Melambangkan keseimbangan dan keselarasan dalam menjalankan organisasi, di mana setiap elemen bekerja bersama menuju tujuan yang sama.</p>
+                    </div>
+                </div>
+                <div class="flsfcards">
+                    <div class="cardtop">
+                        <img src="/images/compro/compro_img7.jpg" alt="shape" id="shape">
+                        <h3>Colors</h3>
+                    </div>
+                    <div class="cardbottom">
+                        <p>Merepresentasikan nilai-nilai yang diusung HIMTI 2025, seperti kebersamaan, inovasi, dan profesionalisme dalam setiap langkah yang diambil.</p>
+                    </div>
+                </div>
+                <div class="flsfcards">
+                    <div class="cardtop">
+                        <img src="/images/compro/compro_img8.jpg" alt="shape" id="shape">
+                        <h3>Visual Elements</h3>
+                    </div>
+                    <div class="cardbottom">
+                        <p>Setiap garis, bentuk, dan simbol dalam logo memiliki arti mendalam yang menggambarkan semangat “Beraksi dalam Kolaborasi untuk Inovasi”, di mana HIMTI tidak hanya bergerak maju, tetapi juga menciptakan dampak nyata bagi lingkungan sekitarnya.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    @include('components.footer')
+
+    <script src="js/compro.js"></script>
 </body>
 </html>
