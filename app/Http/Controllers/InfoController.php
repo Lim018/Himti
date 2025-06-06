47fbb472864c9f5b2cd078feaf36767e094b24f2
Habib Anwash <tesakun6464@gmail.com>
2025-06-06 09:46:39 +0700

    add new model "info"

---
+++ app/Http/Controllers/InfoController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Info; // Import your Info model
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage; // For file storage
+
+class InfoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public static function index()
+    {
+        // Retrieve all infos, ordered by creation date descending
+        $infos = Info::orderBy('date', 'desc')->get();
+
+        // Return a view with the list of infos
+        return $infos;
+    }
+
+    public static function getLatestInfo($number = 1) 
+    {
+        $infos = Info::orderBy('date', 'desc')
+            ->take($number)
+            ->get()
+            ->map(function ($item) {
+                $item->date = \Carbon\Carbon::parse($item->date)->locale('id')->translatedFormat('j F Y');
+                unset($item->created_at);
+                unset($item->updated_at);
+                return $item;
+            })
+            ->toArray();
+        
+        return $infos;
+    }
+}
